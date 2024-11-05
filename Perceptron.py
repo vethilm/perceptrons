4061ac7deb01dbe17d9a272444f7ec94bd81f7f4
vethilm <burnsc376@gmail.com>
2024-11-05 10:31:59 -0500

    added upadte weights

--- Perceptron.py
+++ Perceptron.py
@@ -6,22 +6,31 @@ class Perceptron:
         #initialize weights as random numbers -0.5 - .0.5
         self.x=x
         self.y=y
-        self.w = []
-        for i in range(5):
-            self.w.append(random.randrange(-5,5)/10 )
+        self.w = [-0.5, 0.7, -0.2, 0.1, 0.9]
+       # for i in range(5):
+        #    self.w.append(random.randrange(-5,5)/10 )
         self.alpha = 0.1
 
     def weighted_sum(self):
         sum = 0
-        for i in range(4) :
-            sum+= self.x[i+1]*self.w[i+1]
-        sum -= self.x[0]*self.w[0]
-        sum = round(sum,1)
+        for i in range(5) :
+            sum+= self.x[i]*self.w[i]
+        #sum -= self.x[0]*self.w[0]
+       # sum = round(sum,1)
+        print("sum: ", sum)
         return sum
     
     def step(self):
-        return 0 if self.weighted_sum(self) <=0 else 1
+        return 1 if self.weighted_sum() > 0 else 0 
 
     def predict(self):
-        return 
+        while True:
+            e = self.y - self.step()
+            print(self.y, "- ", self.step(), "=",e)
+            if (e == 0):
+                return
+            for i in range(4):
+                self.w[i] = self.w[i] + (e * self.x[i])
+            print("updated weights", self.w)
+        
 
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import Perceptron as P
+import os
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear_console()
 
 # initilize input values, wehre X[0] is the bias
-x = [1,0,0,1,1]
+x = [1,-1,1,1,-1]
 y = 1
 p = P.Perceptron(x,y)
 
 print("Weights: " ,p.w)
-
-print(p.weighted_sum())
+p.predict()
