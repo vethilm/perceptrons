79730099c8e9f22921c8507c89823dd5a9bb78a6
vethilm <burnsc376@gmail.com>
2024-11-05 11:27:21 -0500

    completed predict method

--- Perceptron.py
+++ Perceptron.py
@@ -3,9 +3,9 @@ import random
 
 class Perceptron:
     def __init__(self, x, y):
-        #initialize weights as random numbers -0.5 - .0.5
         self.x=x
         self.y=y
+        #initialize weights as random numbers -0.5 - .0.5
         self.w = [-0.5, 0.7, -0.2, 0.1, 0.9]
        # for i in range(5):
         #    self.w.append(random.randrange(-5,5)/10 )
@@ -13,24 +13,26 @@ class Perceptron:
 
     def weighted_sum(self):
         sum = 0
-        for i in range(5) :
+        for i in range(len(self.x)) :
             sum+= self.x[i]*self.w[i]
         #sum -= self.x[0]*self.w[0]
        # sum = round(sum,1)
-        print("sum: ", sum)
         return sum
     
     def step(self):
-        return 1 if self.weighted_sum() > 0 else 0 
+        return 1 if self.weighted_sum() > 0 else -1 
 
     def predict(self):
         while True:
-            e = self.y - self.step()
-            print(self.y, "- ", self.step(), "=",e)
+            e = self.alpha * (self.y - self.step())
+            print(self.y, "-", self.step(), "=",e)
             if (e == 0):
                 return
-            for i in range(4):
-                self.w[i] = self.w[i] + (e * self.x[i])
+            for i in range(len(self.x)):
+                self.w[i] = round(self.w[i] + (e * self.x[i]),2)
+
             print("updated weights", self.w)
-        
+
+    def test(self):
+        return
 
